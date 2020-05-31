7ad88ce33c3d73b8d966d82d10bf6c76173e505d
Chinyelu <chinyelulyon@gmail.com>
2020-05-31 13:06:05 +0100

    fitbit integration not working testing tokens 4

--- index.js
+++ index.js
@@ -303,8 +303,10 @@ app.route('/connectFitbit').get(function(req,res){
 });
 
 app.post('/requestFitbit', function(req, res){
+	console.log('Fitbit Access Token: '+ req.body.postData);
+	/*
 	var headers = {
-		'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMkJRUkYiLCJzdWIiOiI3UjlRQ0ciLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJyc29jIHJhY3QgcnNldCBybG9jIHJ3ZWkgcmhyIHJudXQgcnBybyByc2xlIiwiZXhwIjoxNTkwNTI4NjU5LCJpYXQiOjE1OTA0NDk3MTh9.diTppqYOO4wkVbV9Ht0JEKvRCRQBqDgO1cgZx0rVMs8'
+		'Authorization': 'Bearer '+req.body.postData
 	};
 	var options = {
 		url: 'https://api.fitbit.com/1/user/-/profile.json',
@@ -316,7 +318,7 @@ app.post('/requestFitbit', function(req, res){
 		}
 	}
 	request(options, callback);
-
+*/
 	request({
 		headers: {
 			'Authorization': 'Bearer '+req.body.postData
