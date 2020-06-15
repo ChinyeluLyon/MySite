6e645c88891bb7d6e0a5e4520016e59bdca455c5
Chinyelu <chinyelulyon@gmail.com>
2020-06-15 16:03:50 +0100

    activity NOT data obtainted succesfully, checking index.js 4

--- index.js
+++ index.js
@@ -333,127 +333,127 @@ app.get('/getFitbitActivitiesData', function(req, res){
 	}, function (err, res, body) {
 		var activityData = JSON.parse(body);
 		console.log("activityData: "+activityData.summary);
-		console.log("ACTIVITY keys: "+Object.keys(activityData));
+		console.log("ACTIVITY keys: "+Object.keys(activityData.summary));
 		sendData = activityData.summary;
-		res.send({
-			activitySummary: sendData
-		});
 	});
 
+	res.send({
+		activitySummary: sendData
+	});
 });
 
-app.post('/ajaxSignUp', function (req, res){  
+	app.post('/ajaxSignUp', function (req, res){  
 
-	if(!req.body.Name || !req.body.DOB || !req.body.Gender){
-		console.log('Must fill all user fields!!');
-	}
-	else{
-		const token = generateAccessToken({ username: req.body.Name });
-		console.log('token: '+ token);
-		res.json(token);
+		if(!req.body.Name || !req.body.DOB || !req.body.Gender){
+			console.log('Must fill all user fields!!');
+		}
+		else{
+			const token = generateAccessToken({ username: req.body.Name });
+			console.log('token: '+ token);
+			res.json(token);
 
-		var sqlQuery = '\
-		INSERT IGNORE INTO \
-		users (user_name, user_DOB, user_gender, user_password, login_token) \
-		VALUES ("'+req.body.Name+'", "'+req.body.DOB+'", "'+req.body.Gender+'", "'+req.body.Password+'", "'+token+'");\
-		';
-		connection.query(sqlQuery, function (err, rows, fields) {
-			if (err) {
-				throw err;
-				console.log('The solution is: ', rows[0].solution);
-			}
-			else
-			{
-				console.log('New User Signed Up Successfully!');
-			}
-		});
-	}
-});
-
-app.get('/ajaxLogin', function(req, res){
-	console.log('name: '+ req.query.Name);
-	console.log('password: '+ req.query.Password);
-
-	var sqlQuery = 'SELECT * FROM users WHERE user_name = "'+ req.query.Name +'"';
-	connection.query(sqlQuery, function(err, rows, fields){
-		if(err){
-			throw err;
-		}else{
-			if(rows.length <= 0){
-
-				console.log(Object.keys(rows));
-				console.log('wrong username');
-				res.send({
-					Message: 'Incorrect username or password, please try again'
-				});
-			}
-			else{
-				var sqlQuery = 'SELECT user_name, user_DOB, user_gender, user_id, user_password FROM users WHERE user_name = "'+ req.query.Name +'"';
-				connection.query(sqlQuery, function(err, rows, fields){
-					if(err){
-						throw err;
-					}
-					else{
-						if(rows[0].user_password == req.query.Password){
-							const token = generateAccessToken({ username: req.query.Name });
-							console.log('token: '+ token);
-
-							var userId = rows[0].user_id;
-							console.log('UPDATE users SET login_token = "'+ token +'" WHERE user_name = "'+req.query.Name+'"');
-							connection.query('UPDATE users SET login_token = "'+ token +'" WHERE user_name = "'+req.query.Name+'"', function(err, rows, fields){
-								if(err){
-									throw err;
-								}
-								else{
-									res.send({
-										id: userId,
-										JWT: token
-									});
-
-									console.log(req.query.Name + '\'s token was updated to ' + token);
-									console.log('log in Successful');
-								}
-							});
-
-						}
-						else{
-							res.send({
-								Message: 'Incorrect username or password, please try again'
-							});
-							console.log('incorrect password!');
-						}
-					}
-				});
-			}
+			var sqlQuery = '\
+			INSERT IGNORE INTO \
+			users (user_name, user_DOB, user_gender, user_password, login_token) \
+			VALUES ("'+req.body.Name+'", "'+req.body.DOB+'", "'+req.body.Gender+'", "'+req.body.Password+'", "'+token+'");\
+			';
+			connection.query(sqlQuery, function (err, rows, fields) {
+				if (err) {
+					throw err;
+					console.log('The solution is: ', rows[0].solution);
+				}
+				else
+				{
+					console.log('New User Signed Up Successfully!');
+				}
+			});
 		}
 	});
 
+	app.get('/ajaxLogin', function(req, res){
+		console.log('name: '+ req.query.Name);
+		console.log('password: '+ req.query.Password);
 
-});
-
-app.get('/ajaxCheckLoggedIn', function(req, res){
-	if(!req.query.currentToken || req.query.currentToken == 'token='){
-		console.log('not logged In');
-	}
-	else{
-		console.log('token: '+ req.query.currentToken);
-		var token = req.query.currentToken.split('=')[1];
-
-		var sqlQuery = 'SELECT user_name, user_DOB, user_gender, user_id, user_password FROM users WHERE login_token = "'+token+'"';
+		var sqlQuery = 'SELECT * FROM users WHERE user_name = "'+ req.query.Name +'"';
 		connection.query(sqlQuery, function(err, rows, fields){
 			if(err){
 				throw err;
 			}else{
-				res.send({loggedInAs: rows[0].user_name});
+				if(rows.length <= 0){
+
+					console.log(Object.keys(rows));
+					console.log('wrong username');
+					res.send({
+						Message: 'Incorrect username or password, please try again'
+					});
+				}
+				else{
+					var sqlQuery = 'SELECT user_name, user_DOB, user_gender, user_id, user_password FROM users WHERE user_name = "'+ req.query.Name +'"';
+					connection.query(sqlQuery, function(err, rows, fields){
+						if(err){
+							throw err;
+						}
+						else{
+							if(rows[0].user_password == req.query.Password){
+								const token = generateAccessToken({ username: req.query.Name });
+								console.log('token: '+ token);
+
+								var userId = rows[0].user_id;
+								console.log('UPDATE users SET login_token = "'+ token +'" WHERE user_name = "'+req.query.Name+'"');
+								connection.query('UPDATE users SET login_token = "'+ token +'" WHERE user_name = "'+req.query.Name+'"', function(err, rows, fields){
+									if(err){
+										throw err;
+									}
+									else{
+										res.send({
+											id: userId,
+											JWT: token
+										});
+
+										console.log(req.query.Name + '\'s token was updated to ' + token);
+										console.log('log in Successful');
+									}
+								});
+
+							}
+							else{
+								res.send({
+									Message: 'Incorrect username or password, please try again'
+								});
+								console.log('incorrect password!');
+							}
+						}
+					});
+				}
 			}
 		});
-	}
 
-});
 
-app.listen(process.env.PORT || 80, function(){
-	console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
+	});
 
-console.log('server running');
+	app.get('/ajaxCheckLoggedIn', function(req, res){
+		if(!req.query.currentToken || req.query.currentToken == 'token='){
+			console.log('not logged In');
+		}
+		else{
+			console.log('token: '+ req.query.currentToken);
+			var token = req.query.currentToken.split('=')[1];
+
+			var sqlQuery = 'SELECT user_name, user_DOB, user_gender, user_id, user_password FROM users WHERE login_token = "'+token+'"';
+			connection.query(sqlQuery, function(err, rows, fields){
+				if(err){
+					throw err;
+				}else{
+					res.send({loggedInAs: rows[0].user_name});
+				}
+			});
+		}
+
+	});
+
+	app.listen(process.env.PORT || 80, function(){
+		console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+	});
+
+	console.log('server running');
 //var server = app.listen(8080,function() {});
