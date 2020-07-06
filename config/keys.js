6ea459e98d801ed6be2e81d4da0370312163635c
Chinyelu <chinyelulyon@gmail.com>
2020-07-06 17:37:29 +0100

    attempting google login API 2

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
 // add this file to .gitignore
 
-module.export({
+module.exports({
 	google:{
 		clientID: '372579444113-p2shbivf1b1ddrt0i5vtenfv3kh61f2o.apps.googleusercontent.com',
 		clientSecret: 'BLzWam5ve6lRmPbquAqRU-Gd'
