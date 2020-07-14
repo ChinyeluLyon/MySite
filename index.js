ce2e499c890b96e8394d377aa807ff8779336e11
Chinyelu <chinyelulyon@gmail.com>
2020-07-14 13:05:37 +0100

    added browserify to get chart.js managed to get chart of user data

--- index.js
+++ index.js
@@ -60,32 +60,40 @@ let connection = mysql.createConnection({
 connection.connect()
 */
 
-function handleDisconnect() {
-let newConnection = mysql.createConnection({
-	host: 'eu-cdbr-west-03.cleardb.net',
-	user: 'bcc861a75b94d1',
-	password: '7a2672e3',
-	database: 'heroku_b301eebc16a43c7'
-})
 
-  newConnection.connect(function(err) {              // The server is either down
-    if(err) {                                     // or restarting (takes a while sometimes).
-      console.log('error when connecting to db:', err);
-      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
-    }                                     // to avoid a hot loop, and to allow our node script to
-  });                                     // process asynchronous requests in the meantime.
-                                          // If you're also serving http, display a 503 error.
-  newConnection.on('error', function(err) {
-    console.log('db error', err);
-    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
-      handleDisconnect();                         // lost due to either server restart, or a
-    } else {                                      // connnection idle timeout (the wait_timeout
-      throw err;                                  // server variable configures this)
-    }
-  });
+let newConn;
+
+function handleDisconnect() {
+	newConn = mysql.createConnection(mysql.createConnection({
+		host: 'eu-cdbr-west-03.cleardb.net',
+		user: 'bcc861a75b94d1',
+		password: '7a2672e3',
+		database: 'heroku_b301eebc16a43c7'
+	}))
+
+
+
+	newConn.connect(function(err) {              
+		if(err) {      
+			console.log('in here')                              
+			console.log('error when connecting to db:', err);
+			setTimeout(handleDisconnect, 2000); 
+		}                                     
+	});                                   
+	newConn.on('error', function(err) {
+		console.log('db error', err);
+		if(err.code === 'PROTOCOL_CONNECTION_LOST') {
+			handleDisconnect();                         
+		} else {                                     
+			throw err;
+		}
+	});
 }
 
-handleDisconnect(connection);
+handleDisconnect();
+
+
+
 
 //home page
 app.route("/").get(function(req,res)
