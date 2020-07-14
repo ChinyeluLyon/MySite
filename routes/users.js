51e63c02df9997357ea6bb5abedbecdb582616d5
Chinyelu <chinyelulyon@gmail.com>
2020-07-14 14:00:30 +0100

    fixed error in userProfile.js, user image viewable now, the old user page has been removed

--- routes/users.js
+++ routes/users.js
@@ -36,53 +36,7 @@ router.route("/users").get(function(req,res)
 
 })
 
-// router.route('/user').get(authenticateToken, function(req,res)
-// {
-// 	console.log('SELECT * FROM users WHERE login_token = "'+req.cookies.token+'"')
-// 	let sqlQuery = 'SELECT * FROM users WHERE login_token = "'+req.cookies.token+'"'
-// 	connection.query(sqlQuery, function (err, rows, fields) {
-// 		if (err) {
-// 			throw err
-// 			console.log('The solution is: ', rows[0].solution)
-// 		}
-// 		else{
-// 			let nowDate = new Date()
-// 			let age = new Date(nowDate - rows[0].user_DOB)
 
-// 			res.render('user', 
-// 			{
-// 				Name: rows[0].user_name,
-// 				Age: age.getUTCFullYear() - 1970
-// 			})
-// 		}
-// 	})
-// })
-
-
-//this is a new webpage, should show specific user
-router.route('/users/:ID').get(function(req,res)
-{
-	let sqlQuery = 'SELECT * FROM users WHERE user_id = ?'
-	connection.query(sqlQuery, [req.params.ID], function (err, rows, fields) {
-		if (err) {
-			throw err
-			console.log('The solution is: ', rows[0].solution)
-		}
-		else if (rows.length > 0){
-			res.render('user', 
-			{
-				Name: rows[0].user_name, 
-				DOB: rows[0].user_DOB, 
-				Gender: rows[0].user_gender,
-				ID: rows[0].user_id 
-			})
-			console.log('Query Successful!')
-		}
-		else{
-			res.render('home')
-		}
-	})
-})
 
 const checkIfLoggedInAuth = (req, res, next)=>{
 	if (!req.user){
