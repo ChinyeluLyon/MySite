86707297600a628351c4d74bc825e738c699cc10
Chinyelu <chinyelulyon@gmail.com>
2020-07-24 20:35:04 +0100

    merging problems fixed i think

--- routes/gallery.js
+++ routes/gallery.js
@@ -6,21 +6,9 @@ let db = require('../database');
 router.route("/gallery").get(function(req,res)
 {
 	let sqlQuery = 'SELECT * FROM users'
-<<<<<<< HEAD
-	db.query(sqlQuery, function (err, rows, fields) {
-		if (err) {
-			throw err
-			console.log('The solution is: ', rows[0].solution)
-		}
-		else{
-			res.render('gallery', {pageName: 'Gallery'})
-			console.log('Gallery Opened')
-		}
-=======
 	pool.useMysqlPool(sqlQuery, function(rows){
 		res.render('gallery', {pageName: 'Gallery'})
 		console.log('Gallery Opened')
->>>>>>> origin/master
 	})
 })
 
