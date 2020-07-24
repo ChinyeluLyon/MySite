483f010831a7e34d22960432a0bf59dc30f84610
Chinyelu <chinyelulyon@gmail.com>
2020-07-24 20:11:41 +0100

    recent update changes arent reflected in heroku, re pushing

---
+++ routes/home.js
@@ -0,0 +1,34 @@
+const pug = require('pug')
+const express = require('express')
+const router = express.Router()
+let pool = require('../database')
+
+router.route("/").get(function(req,res)
+{	
+	console.log(req.user)
+	if(req.user)
+	{
+		console.log('logged in')
+		console.log('DEBUG RECENT UPDATE')
+
+		let getUserImageSQL = 'SELECT user_image_url from new_users WHERE user_id = '+req.user+' LIMIT 1'
+		pool.useMysqlPool(getUserImageSQL, function(rows){
+			res.render('home', 
+			{
+				pageName: 'Home',
+				userID: req.user,
+				userImage: rows[0].user_image_url
+			})
+		})
+
+	}
+	else{
+		res.render('home', 
+		{
+			pageName: 'Home'
+		})
+	}
+})
+
+
+module.exports = router
